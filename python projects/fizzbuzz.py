b3a6d2acc58e04c58aacfa1aadb8aaebcfe64ea8
microbot30 <18eucs042@skcet.ac.in>
2020-08-25 17:35:48 +0530

    TalentSprint project!

--- python projects/fizzbuzz.py
+++ python projects/fizzbuzz.py
@@ -1,9 +1,13 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FIZZBUZZ", end=' ')
-    elif i % 3 == 0:
-        print("FIZZ", end=' ')
-    elif i % 5 == 0:
-        print("BUZZ", end=' ')
+def fizzbuzz(n: int) -> str:
+    if n % 3 == 0 and n % 5 == 0:
+        return "FIZZBUZZ"
+    elif n % 3 == 0:
+        return "FIZZ"
+    elif n % 5 == 0:
+        return "BUZZ"
     else:
-        print(i, end=' ')
+        return str(n)
+
+
+for i in range(1, 101):
+    print(fizzbuzz(i), end=' ')
